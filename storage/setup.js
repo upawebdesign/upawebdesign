c98b7ac4631f69f5f447f7dda2d4192af58986b3
Ishan Madan <ishan@mistlogic.com>
2019-08-19 23:10:17 -0700

    added footer file, updated setup.js

--- storage/setup.js
+++ storage/setup.js
@@ -1,7 +1,6 @@
 var head = '<link rel="stylesheet" type="text/css" href="/storage/stylesheet.css">';
-var header = `<a href="/index.html"><h1 id='title'>University Preparatory Academy</h1></a>
-<h2>Header</h2>`;
 $(function(){
     $('head').append(head);
-    $('#header').append(header);
+    $('#header').load('/storage/header.html');
+    $('#footer').load('/storage/footer.html');
 });
